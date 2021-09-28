7c313abe7e6091cf9f56b622f5bda779c8198700
mayara bueno <luziamayara83@gmail.com>
2021-09-28 14:46:29 -0300

    [Feature Mayara - Task: Criação componente rodape ]

--- ClimatikaStore/src/app/app.module.ts
+++ ClimatikaStore/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,

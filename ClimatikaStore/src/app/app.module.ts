4e9d2bd70f03c62f7447610ef164a1706a918ac2
Agatha Nara <gthlbrc@outlook.com>
2021-09-29 11:32:45 -0300

    [Feature Agatha - Task: Pagina Entrar]

--- ClimatikaStore/src/app/app.module.ts
+++ ClimatikaStore/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,

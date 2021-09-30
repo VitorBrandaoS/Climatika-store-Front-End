9beaad29a072fd17b7fb2b3d154dd38a7d5beb43
unknown <ferreirasousajaqueline@gmail.com>
2021-09-29 21:30:58 -0300

    [Feature Jaqueline - Task: Implementando cadastro]

--- ClimatikaStore/src/app/app.module.ts
+++ ClimatikaStore/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    EntrarComponent
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,

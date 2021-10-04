0822a4288d3bc5a28f42a303d5e636fbd5863a44
Agatha Nara <gthlbrc@outlook.com>
2021-10-04 09:19:02 -0300

    [Feature Agatha - Task: Add Services]

--- ClimatikaStore/src/app/app.module.ts
+++ ClimatikaStore/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { InicioComponent } from './inicio/inicio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ClimatikaStore/src/app/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+    return this.http.post<UsuarioLogin>('https://climatika-store-ecommerce.herokuapp.com/climatika/usuario/login', usuarioLogin)
+
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://climatika-store-ecommerce.herokuapp.com/climatika/usuario/cadastrar', usuario)
+
+  }
+}

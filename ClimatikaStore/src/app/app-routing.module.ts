fb69fe6d73652f749cab8cd32d28b6aa9a205b59
unknown <ferreirasousajaqueline@gmail.com>
2021-10-04 11:28:51 -0300

    [Feature Jaqueline - Task: Implementação das rotas]

--- ClimatikaStore/src/app/app-routing.module.ts
+++ ClimatikaStore/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+
+{path:'entrar', component: EntrarComponent},
+{path:'cadastrar', component: CadastrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

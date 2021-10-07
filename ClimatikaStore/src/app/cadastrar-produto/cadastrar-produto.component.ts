1c1148dfed49490231e67e2707702843c93483bf
isaiaszanoni <isaiasz.mus@gmail.com>
2021-10-07 11:51:04 -0300

    [Feature Isaias - Task: Criacao do componente Cadastrar Produto]

---
+++ ClimatikaStore/src/app/cadastrar-produto/cadastrar-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-produto',
+  templateUrl: './cadastrar-produto.component.html',
+  styleUrls: ['./cadastrar-produto.component.css']
+})
+export class CadastrarProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

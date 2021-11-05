051ae617e15beb8ac6cbd0998e114519aacc159c
Vitor Brandão <vitorbrandaosilva@outlook.com>
2021-11-05 13:50:52 -0300

    [Feature Vitor - Task: Correções em cadastrar usuario Completa]

--- ClimatikaStore/src/app/cadastrar/cadastrar.component.ts
+++ ClimatikaStore/src/app/cadastrar/cadastrar.component.ts
@@ -16,6 +16,7 @@ export class CadastrarComponent implements OnInit {
 
   usuario: Usuario = new Usuario()
   confirmarSenha: string
+  celular: string
   idVenda: number
   statusVenda: StatusVenda = new StatusVenda()
 
@@ -34,6 +35,10 @@ export class CadastrarComponent implements OnInit {
     this.confirmarSenha = event.target.value
   }
 
+  confirmCelular(event: any) {
+    this.celular = event.target.value
+  }
+
   openStatusVenda(statusVenda: StatusVenda){
     this.carrinhoService.criarStatusVenda(statusVenda).subscribe((resp: StatusVenda) => {
       this.statusVenda = resp
@@ -50,20 +55,32 @@ export class CadastrarComponent implements OnInit {
   cadastrar() {
     if (this.usuario.senha != this.confirmarSenha) {
       alert("As senhas estão incorretas.")
+    }else if(this.confirmarSenha.length < 5 || this.usuario.senha.length < 5){
+      alert("Atenção! Sua senha precisa ter no mínimo 5 caracteres!")
+    } else if(this.celular.length != 11){
+      alert("São apenas 11 dígitos que aceitamos para o cadastro do seu celular!")
     } else {
       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {        
         this.usuario = resp
+
+        alert("Usuario cadastrado com sucesso!")
+          this.router.navigate(["/entrar"])
+
+          console.log(this.usuario.id)
+          console.log(this.usuario.nomeCompleto)
+          console.log(this.usuario.email)
+          console.log(this.usuario.senha)
+          console.log(this.usuario.tipoUsuario)
+          console.log(this.usuario.celular)
+          console.log(this.usuario.cidade)
+          console.log(this.usuario.estado)
+      },erro =>{
+        if(erro.status == 500){
+          alert('Algo deu errado! Por favor, confira seus dados e tente novamente.')
+        }if(erro.status == 400){
+          alert('Algo deu errado! Por favor, confira seus dados e tente novamente.')
+        }
       })
-      console.log(this.usuario.id)
-      console.log(this.usuario.nomeCompleto)
-      console.log(this.usuario.email)
-      console.log(this.usuario.senha)
-      console.log(this.usuario.tipoUsuario)
-      console.log(this.usuario.celular)
-      console.log(this.usuario.cidade)
-      console.log(this.usuario.estado)
-      alert("Usuario cadastrado com sucesso!")
-      this.router.navigate(["/entrar"])
     }
   }
 
